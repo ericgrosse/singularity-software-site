f14d92e3b88f3b7202570ec66c3f620e67d5340b
ericgrosse <ericgrosse1@gmail.com>
2024-08-26 20:17:54 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Singularity Software Website
+
+- TODO
---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Header</h1>
+        <p>
+          Vykan12 was here - Singularity Software
+        </p>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Master JavaScript
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
